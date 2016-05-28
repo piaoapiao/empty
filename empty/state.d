6746e8e37c6eaab71a576e8e1b1ff7768df7c4bf
huayu0723 <zhyhua04@gmail.com>
2016-05-28 16:07:08 +0800

    add one more probe scripts

--- empty/state.d
+++ empty/state.d
@@ -1,6 +1,16 @@
+uint64_t last_state_timestamp;
+
 syncengine_sync*:::strategy_go_to_state
 {
     printf("Transitioning to state %d\n", arg0);
 }
+
+syncengine_sync*:::strategy_go_to_state
+{
+    t = (walltimestamp - last_state_timestamp) / 1000000;
+    last_state_timestamp = walltimestamp;
+    printf("Spent %d ms\n", t);
+}
+
 
 
